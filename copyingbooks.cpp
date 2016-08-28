fd9596ea52e168720638d0adfa0d13c7304e72f2
Vanessa Vieira <vsv@ic.ufal.br>
2016-08-28 20:14:52 -0300

    does nothing more than read the input

---
+++ copyingbooks.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 10000
+
+typedef int array[MAX];
+int j = 0;
+
+int main()
+{
+    int n_cases, books, scribers;
+
+    scanf("%d", &n_cases);
+
+    for (int i = 0; i < n_cases; ++i)
+    {
+        scanf("%d %d", &books, %scribers);
+
+        array pages;
+
+        for (j = 0; j < books; ++j)
+        {
+            scanf("%d", &pages[j]);
+        }
+    }
+    return 0;
+}
