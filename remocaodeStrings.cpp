0756ab1dda21ebedac1a409a374e5ab929465a3b
Vanessa Vieira <vsv@ic.ufal.br>
2016-10-11 01:23:30 -0300

    Funcionando com DP

--- remocaodeStrings.cpp
+++ remocaodeStrings.cpp
@@ -1,91 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<string> results;
-struct indexes {
-    int groups_index_i;
-    int groups_index_j;
-};
+map<string, int> dp;
 
-
-
-int searchString(vector<string> results, string N)
+int backtracking(string n)
 {
-    //found
-    if (std::find(results.begin(), results.end(), N) != results.end())
+    if(dp.count(n))return dp[n];
+
+    int result = 0;
+    vector<int> next(n.size());
+
+    next[n.size()-1] = n.size()-1;
+    // separa grupos
+    for(int i = n.size()-2; i >= 0; --i)
     {
-        return 1;
-    }
-    else
-    {
-        printf("nao achou\n");
-        return -1;
-    }
-
-}
-
-string concatenate(string n, indexes indices)
-{
-    string temp;
-
-    for (int i = 0; i < indices.groups_index_i; i++)
-    {
-        temp.push_back(n[i]);
-    }
-    for (int j = indices.groups_index_j + 1; j < n.length(); j++)
-    {
-        temp.push_back(n[j]);
-    }
-    cout << "concatenado: " << temp << "\n";
-    return temp;
-
-  //TOP
-}
-
-vector<indexes> findGroup(string N)
-{
-    vector<indexes> indices;
-    indexes new_index;
-
-    int j = 0;
-    int k = 0;
-
-    for (int i = 0; i < N.length(); ++i)
-    {
-        if (N[i] != N[j])
+        if(n[i] == n[i+1])
         {
-            k = i - 1;
-            if (k - j >= 1)
-            {
-                new_index.groups_index_i = j;
-                new_index.groups_index_j = k;
-                indices.push_back(new_index);
-            }
-            j = i;
+            next[i] = next[i+1];
         }
+        else next[i] = i;
     }
 
-    return indices;
-}
+    // "concatenando"
+    for(int i = 0; i < next.size(); i = next[i]+1)
+    {
+        if (i != next[i])
+        {
+            string conc = n;
+            conc.erase(i, next[i]-i+1);
+            result = result || backtracking(conc);
+        }
 
-
-bool backtracking(string n)
-{
-  // TODO AFTER: VERIFICAR SE JA NAO TEM RESPOSTA NO DP
-  // CASO VERDADEIRO: busca o resultado no array
-  // CASO FALSO: calcula e faz isso aqui de baixo \/
-
-  vector<indexes> indices = findGroup(n);
-  bool result = false;
-
-  for (int i = 0; i < indices.size(); i++) {
-    // remove a substring e adiciona
-    string nova_string = concatenate(n, indices[i]);
-    result = result || backtracking(nova_string);
-  }
-
-  // CASO O RESULTADO SEJA VERDADEIRO, ADICIONA STRING NO RESULTS
-  return result;
+    }
+    return dp[n] = result;
 }
 
 int main()
@@ -94,19 +41,24 @@ int main()
     scanf("%d", &T);
     getchar();
 
+    dp["a"] = dp["b"] = 0;
+    dp[""] = 1;
 
     while (T--)
     {
         string N;
         getline(cin, N);
 
-		  bool final_result = backtracking(N);
+        int final_result = backtracking(N);
 
-      if (final_result) {
-        cout << 1 << endl;
-      } else {
-        cout << 0 << endl;
-      }
+        if (final_result == 1)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 0 << endl;
+        }
     }
     return 0;
 }
